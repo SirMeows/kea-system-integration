e69f93af3ad7947cd6f472f50c2f9ffe9f46fb89
SirMeows <sirmeowsh@gmail.com>
2025-04-11 16:15:12 +0200

    add proper handling of uploads directory with .gitkeep

--- SI_00_classroom/SI_multipart_form/main.py
+++ SI_00_classroom/SI_multipart_form/main.py
@@ -1,7 +1,29 @@
+import os
+import re
 from fastapi import FastAPI, Form, File, UploadFile
 import aiofiles
 from typing import Optional
 from datetime import datetime
+from uuid_extensions import uuid7str
+
+# Looks for environment variable for upload directory
+# If not found, creates default 
+UPLOAD_DIR = os.path.abspath(os.getenv("UPLOAD_DIR", "./uploads"))
+
+def handle_upload_directory():
+    # Create folder if it doesn't exist
+    # Only create .gitkeep if folder was freshly created
+    if not os.path.exists(UPLOAD_DIR):
+        os.makedirs(UPLOAD_DIR, exist_ok=True)
+        with open(os.path.join(UPLOAD_DIR, ".gitkeep"), "w") as f:
+            pass
+
+handle_upload_directory()
+
+def generate_safe_filename(original_filename: str) -> str:
+    safe_name = re.sub(r'[^\w\-_\.]', '_', original_filename)
+    unique_id = uuid7str()
+    return f"{unique_id}__{safe_name}"
 
 app = FastAPI()
 
@@ -12,11 +34,10 @@ def basic_form(username: str = Form(...), password: str = Form(default=..., min_
 
 @app.post("/fileform")
 async def file_form(file: UploadFile = File(...), description: Optional[str] = None):
-    safe_filename = file.filename.replace("/", "_").replace("\\", "_")
-    unique_filename = str(datetime.now()) + "__" + safe_filename
+    unique_filename = generate_safe_filename(file.filename)
+    file_path = os.path.join(UPLOAD_DIR, unique_filename)
+    print(file_path)
 
-
-    async with aiofiles.open("./uploads/"+unique_filename, "wb") as f:
-        # := is the walrus operator
+    async with aiofiles.open(file_path, "wb") as f:
         while content := await file.read(1024): # read in chunks of 1024
             await f.write(content)
