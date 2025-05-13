c9cbbc18a772907b935dd04bee7e430dfe758f80
SirMeows <sirmeowsh@gmail.com>
2025-05-13 21:33:34 +0200

    add documentation of auth integration

--- SI_13a_graphql/src/main/java/com/sirmeows/graphql/GraphqlApplication.java
+++ SI_13a_graphql/src/main/java/com/sirmeows/graphql/GraphqlApplication.java
@@ -2,7 +2,6 @@ package com.sirmeows.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.graphql.ConditionalOnGraphQlSchema;
 
 @SpringBootApplication
 public class GraphqlApplication {
